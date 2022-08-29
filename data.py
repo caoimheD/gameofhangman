62e29e28eeb8462bdf950d9402cc36a6e0cc10ce
caoimheD <caoimhe.db5@gmail.com>
2022-08-29 09:23:31 +0000

    add list of words to data.py and import list into run.py

---
+++ data.py
@@ -0,0 +1,1 @@
+words = ["welcome", "popcorn", "health", "friends", "winter", "scarf", "meeting", "snake", "bubble", "travel", "holiday", "cheerful", "influence", "observant"]
--- run.py
+++ run.py
@@ -1,3 +1,22 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from data import words
+
+def get_word(words):
+    word = random.choice(words)
+
+    return word
+
+def thegame():
+    letters = set(word)
+    used_letters = set()
+
+    
+
+
+player_input = input('pick a letter')
+print(player_input)
+
+get_word(words)
