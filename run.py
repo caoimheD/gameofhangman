e22e509cc9c7985fecea9e6d488febb520e9d030
caoimheD <caoimhe.db5@gmail.com>
2022-08-30 10:22:54 +0000

    add turns left and play again option

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
 from data import words
 
@@ -9,8 +6,11 @@ def thegame():
     turns = 10
     playerguess = ''
     validchoices = set('abcdefhijklmnopqrstuvwxyz')
+    random_word = "_ " * len(word)
+    guessed_letters = []
+    guessed = False
 
-    while len(word) > 0:
+    while not guessed and turns > 0:
         random_word = ''
 
         for letter in word:
@@ -18,30 +18,46 @@ def thegame():
                 random_word = random_word + letter
             else:
                 random_word = random_word + "_ "
-        
+
         if random_word == word:
             print(random_word)
-            print("you guessed the word!")
+            print("You guessed the word!")
             break
 
         print("Guess the word! ", random_word)
         guess = input()
 
-        if guess in validchoices:
-           playerguess = playerguess + guess
-        else:
-            print('enter a valid character')
-            guess = input()
-
-        if guess not in word:
-            turns = turns - 1
+        if guess in validchoices and len(guess) == 1:
+            if guess in guessed_letters:
+                print('you already tried this letter', guess)
+                turns -= 1
+                print('Turns left: ', turns)
+            elif guess not in word:
+                print(guess, 'is not in the word')
+                turns -= 1
+                guessed_letters.append(guess)
+                print('Turns left: ', turns)
+            elif guess not in validchoices:
+                print('Not a valid guess. Please enter a valid character')
+                guess = input()
+                print('Turns left: ', turns)
+            else:
+                print('Well done!', guess, 'is in the word!')
+                guessed_letters.append(guess)
+                playerguess = playerguess + guess
+            
         if turns == 0:
-            print('game over')
+            print('Game over! The word was', word)
             break
 
+def main():
+    while input("Play again? Y/N ").upper() == "Y":
+        thegame()
 
-getname = input('Enter your name: ')
-print('Hi', getname,'!')
-print('try to guess the word, 10 attempts')
+getname = input('Lets play Hangman! Enter your name: ')
+print('Hi', getname)
+print('Try to guess the word, you have 10 attempts')
+
 
 thegame()
+main()
