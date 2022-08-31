609302fba45639ef36fee26a92a591a6c529a02e
caoimheD <caoimhe.db5@gmail.com>
2022-08-31 19:36:31 +0000

    create graphics function and add graphics

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import random
-from data import words
+
 # importing and initializing colorama, for colour text
 from colorama import init, Fore, Back, Style
 init(autoreset=True)
+
+# imports list of words from data file
+from data import words
 
 # function that gets a random word from the list of words in data file
 def get_word():
@@ -10,7 +13,7 @@ def get_word():
 
     return word
 
-# function that plays the game (while loop that breaks if word guessed or turns is 0)
+# function that plays the game (while loop breaks if word guessed or turns is 0)
 def thegame():
     word = get_word()
     turns = 10
@@ -31,7 +34,7 @@ def thegame():
                 print(Fore.RED + 'you already tried this letter', playerguess, '\n')
                 turns -= 1
                 print('Turns left: ', turns, '\n')
-                print("Letters tried: ", guessed_letters, '\n')            
+                print("Letters tried: ", guessed_letters, '\n')         
             elif playerguess in word:
                 print(Fore.GREEN + "Well done!", playerguess, "is in the word!", '\n')
                 guessed_letters.append(playerguess)
@@ -44,12 +47,14 @@ def thegame():
                 guessed_letters.append(playerguess)
                 print('Turns left: ', turns, '\n')
                 print("Letters tried: ", guessed_letters, '\n')
+                graphics(turns)
                 
         else:
             print(Fore.RED + 'Not a valid character. Enter a letter of the alphabet.')    
             pass
         
         if turns == 0:
+            graphics(turns)
             print(Fore.RED + "Game over! You lost!", "The word was", word)
             break
 
@@ -77,6 +82,52 @@ print('Hi', getname)
 print('Try to guess the word, you have 10 attempts')
 
 # function for graphics
+
+def graphics(turns):
+    if turns == 9:
+        print("--------------")
+    if turns == 8:
+        print("--------------")
+        print("       O      ")
+    if turns == 7:
+        print("--------------")
+        print("       O      ")
+        print("       |      ")
+    if turns == 6:
+        print("--------------")
+        print("       O      ")
+        print("       |      ")
+        print("      /       ")
+    if turns == 5:
+        print("--------------")
+        print("       O      ")
+        print("       |      ")
+        print("      / \     ")
+    if turns == 4:
+        print("--------------")
+        print("      \O      ")
+        print("       |      ")
+        print("      / \     ")
+    if turns == 3:
+        print("--------------")
+        print("      \O/      ")
+        print("       |      ")
+        print("      / \     ")
+    if turns == 2:
+        print("--------------")
+        print("      \O/ |   ")
+        print("       |      ")
+        print("      / \     ")
+    if turns == 1:
+        print("--------------")
+        print("      \O/_|   ")
+        print("       |      ")
+        print("      / \     ")
+    if turns == 0:
+        print(Fore.RED + "--------------")
+        print("      \O/_|   ")
+        print("       |      ")
+        print("      / \     ")
 
 # calling functions
 thegame()
