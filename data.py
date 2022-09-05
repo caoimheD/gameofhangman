92028453b25be8ca0bf5495252f0a3f331f23bbe
caoimheD <caoimhe.db5@gmail.com>
2022-09-05 19:24:31 +0000

    add words to the category lists

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 """
 list of words
 """
-animalwords = ["monkey", "dog"]
+animalwords = ["monkey", "dog", "alligator", "giraffe", "elephant", "kangaroo", "whale", "raccoon", "hamster", "rabbit", "orangutan", "antelope", "leopard", "octopus", "squirrel", "rooster", "chicken", "reindeer"]
 
-geowords = ["london", "paris"]
+geowords = ["london", "paris", "rome", "melbourne", "harare", "japan", "china", "tanzania", "algeria", "istanbul", "ecuador", "sydney", "vietnam", "hamburg", "canada", "washington", "nairobi", "senegal", "prague", "milan"]
 
-foodwords = ["apple", "orange"]
+foodwords = ["apple", "orange", "pizza", "lettuce", "watermelon", "rosemary", "broccoli", "coconut", "cheese", "peppers", "haddock", "marshmallows", "baguette", "carrots", "pumpkins", "mushrooms"]
