2ad4198aa649d4bb3bd48e3f19d8afb0d600cd03
caoimheD <caoimhe.db5@gmail.com>
2022-09-06 18:01:35 +0000

    fix errors in run.py file

--- data.py
+++ data.py
@@ -1,8 +1,16 @@
 """
 list of words
 """
-animalwords = ["monkey", "dog", "alligator", "giraffe", "elephant", "kangaroo", "whale", "raccoon", "hamster", "rabbit", "orangutan", "antelope", "leopard", "octopus", "squirrel", "rooster", "chicken", "reindeer"]
+animalwords = ["monkey", "dog", "alligator", "giraffe", "elephant", "kangaroo",
+               "whale", "raccoon", "hamster", "rabbit", "orangutan", "chicken",
+               "leopard", "octopus", "squirrel", "rooster", "antelope",
+               "reindeer"]
 
-geowords = ["london", "paris", "rome", "melbourne", "harare", "japan", "china", "tanzania", "algeria", "istanbul", "ecuador", "sydney", "vietnam", "hamburg", "canada", "washington", "nairobi", "senegal", "prague", "milan"]
+geowords = ["london", "paris", "rome", "melbourne", "scotland", "japan",
+            "china", "tanzania", "algeria", "istanbul", "ecuador", "sydney",
+            "vietnam", "hamburg", "canada", "washington", "nairobi",
+            "senegal", "prague", "milan"]
 
-foodwords = ["apple", "orange", "pizza", "lettuce", "watermelon", "rosemary", "broccoli", "coconut", "cheese", "peppers", "haddock", "marshmallows", "baguette", "carrots", "pumpkins", "mushrooms"]
+foodwords = ["apple", "orange", "pizza", "lettuce", "watermelon", "rosemary",
+             "broccoli", "coconut", "cheese", "peppers", "haddock",
+             "marshmallows", "baguette", "carrots", "pumpkins", "mushrooms"]
--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from data import animalwords  # imports lists of words from data file
 from data import geowords
 from data import foodwords
 
-import colorama  #importing and initializing colorama, for colour text
+import colorama  # importing and initializing colorama, for colour text
 from colorama import init, Fore, Back, Style
 init(autoreset=True)
 
@@ -20,7 +20,8 @@ def output_area():
         get_name = input('Lets play Hangman! Enter your name: ')
 
     print('\nHi', get_name)
-    print(Fore.BLUE + '\nObjective of the game is to guess a hidden word, you can make 10 incorrect guesses.\n')
+    print(Fore.BLUE + '\nObjective of the game is to guess a hidden word,',
+          'you can make 10 incorrect guesses.\n')
 
 
 class CategoryChoice:
@@ -48,8 +49,11 @@ def get_word():
     """
 
     while True:
-        make_choice = input("Choose your category (enter 1, 2 or 3):\n\n 1. Animals\n 2. Geography\n 3. Food\n")
-        CategoryChoice(make_choice).category_selection()  # uses the function in the CategoryChoice class to print based on player choice
+        make_choice = input("Choose your category (enter 1, 2 or 3):\n\n 1.",
+                            "Animals\n 2. Geography\n 3. Food\n")
+        CategoryChoice(make_choice).category_selection()
+        # uses the function in the CategoryChoice class to print
+        # based on player choice
 
         if make_choice == '1':
             word = random.choice(animalwords)
@@ -63,15 +67,13 @@ def get_word():
         else:
             print('enter only 1, 2 or 3')
             pass
-    
     return word
-
-
 
 
 def the_game():
     """
-    function that plays the game (while loop breaks if word guessed or turns is 0)
+    function that plays the game (while loop breaks if word guessed
+    or turns is 0)
     """
     word = get_word()
     turns = 10
@@ -89,35 +91,39 @@ def the_game():
         if player_guess in valid_choices:
 
             if player_guess in guessed_letters:
-                print(Fore.RED + 'you already tried this letter', player_guess, '\n')
+                print(Fore.RED + 'you already tried this letter', player_guess,
+                      '\n')
                 turns -= 1
                 print('Incorrect guesses left: ', turns, '\n')
-                print("Letters tried: ", guessed_letters, '\n')         
+                print("Letters tried: ", guessed_letters, '\n')
             elif player_guess in word:
-                print(Fore.GREEN + "Well done!", player_guess, "is in the word!", '\n')
+                print(Fore.GREEN + "Well done!", player_guess, "is in the",
+                      "word", '\n')
                 guessed_letters.append(player_guess)
                 print('Incorrect guesses left: ', turns, '\n')
                 print("Letters tried: ", ', '.join(guessed_letters), '\n')
-                display_letter(word, random_word, player_guess)   
+                display_letter(word, random_word, player_guess)
             else:
-                print(Fore.RED + "That's incorrect!", player_guess, "is not in the word", '\n')
+                print(Fore.RED + "That's incorrect!", player_guess, "is not",
+                      "in the word", '\n')
                 turns -= 1
                 guessed_letters.append(player_guess)
                 print('Incorrect guesses left: ', turns, '\n')
                 print("Letters tried: ", ', '.join(guessed_letters), '\n')
                 graphics(turns)
-                
         else:
-            print(Fore.RED + 'Not a valid character. Enter a letter of the alphabet.')    
+            print(Fore.RED + 'Not a valid character. Enter a',
+                  'letter of the alphabet.')
             pass
-        
+
         if turns == 0:
             graphics(turns)
             print(Fore.RED + "Game over! You lost!", "The word was", word)
             break
 
         if "_ " not in random_word:
-            print(Fore.GREEN + "Well done! You guessed the word!", "The word was", word, '\n')
+            print(Fore.GREEN + "Well done! You guessed the word!",
+                  "The word was", word, '\n')
             guessed = True
             break
 
@@ -137,7 +143,7 @@ def display_letter(word, random_word, playerguess):
     for i in range(len(word)):
         if playerguess == word[i]:
             random_word[i] = playerguess
-    
+
     return (" ".join(random_word))
 
 
@@ -191,12 +197,13 @@ def graphics(turns):
         print(Fore.RED + "      / \     ")
 
 
-# calling functions
 def main():
+    """
+    calling all functions
+    """
     output_area()
     the_game()
     play_again()
 
 
 main()
-
